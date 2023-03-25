e00af9d1b4f4cfbd745f5cc83b5541bd53c3d6bf
Pierre <wollpierre@gmail.com>
2023-03-25 19:26:49 +0100

    tp05

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -7,39 +7,65 @@ use \Firebase\JWT\JWT;
 require __DIR__ . '/../vendor/autoload.php';
  
 const JWT_SECRET = "makey1234567";
+$jwt="";
 
 $app = AppFactory::create();
 
+function  addHeaders (Response $response) : Response {
+    $response = $response
+    ->withHeader("Content-Type", "application/json")
+    ->withHeader('Access-Control-Allow-Origin', '*')
+    ->withHeader('Access-Control-Allow-Headers', 'Content-Type,  Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+    ->withHeader('Access-Control-Expose-Headers', 'Authorization');
 
-$app->get('/api/hello/{name}', function (Request $request, Response $response, $args) {
-    $array = [];
-    $array ["nom"] = $args ['name'];
-    $response->getBody()->write(json_encode ($array));
     return $response;
-});
+}
 
 function createJwT (Response $response) : Response {
 
     $issuedAt = time();
     $expirationTime = $issuedAt + 60;
     $payload = array(
-    'userid' => 'toto',
-    'email' => 'titi@gmail.com',
-    'pseudo' => 'titiPseudo',
-    'iat' => $issuedAt,
-    'exp' => $expirationTime
+        'userid' => 'toto',
+        'email' => 'titi@gmail.com',
+        'pseudo' => 'titiPseudo',
+        'iat' => $issuedAt,
+        'exp' => $expirationTime
     );
 
     $token_jwt = JWT::encode($payload,JWT_SECRET, "HS256");
     $response = $response->withHeader("Authorization", "Bearer {$token_jwt}");
+    $GLOBALS['jwt'] = $token_jwt;
     return $response;
 }
+
+$app->options('/api/hello/{name}', function (Request $request, Response $response, $args) {
+    $response = $response->withHeader("Access-Control-Max-Age", 600); 
+    return addHeaders ($response);
+});
+
+$app->get('/api/hello/{name}', function (Request $request, Response $response, $args) {
+    $array = [];
+    $array ["nom"] = $args ['name'];
+    $response->getBody()->write(json_encode ($array));
+    return addHeaders($response);
+});
+
+$app->options('/api/user', function (Request $request, Response $response, $args) {
+    $response = $response->withHeader("Access-Control-Max-Age", 600);
+    return addHeaders ($response);
+});
 
 $app->get('/api/user', function (Request $request, Response $response, $args) {   
     $data = array('nom' => 'toto', 'prenom' => 'titi','adresse' => '6 rue des fleurs', 'tel' => '0606060607');
     $response->getBody()->write(json_encode($data));
+    return addHeaders($response);
+});
 
-    return $response;
+$app->options('/api/login', function (Request $request, Response $response, $args) {
+    $response = $response->withHeader("Access-Control-Max-Age", 600); 
+    return addHeaders($response);
 });
 
 // APi d'authentification générant un JWT
@@ -57,15 +83,44 @@ $app->post('/api/login', function (Request $request, Response $response, $args) 
     }
 
     if (!$err) {
-            $response = createJwT ($response);
-            $data = array('nom' => 'toto', 'prenom' => 'titi');
+            $response = createJwT($response);
+            $data = array('nom' => 'toto', 'prenom' => 'titi','jwt'=>$GLOBALS['jwt']);
             $response->getBody()->write(json_encode($data));
      } else {          
             $response = $response->withStatus(401);
      }
-    return $response;
+    return addHeaders($response);
 });
 
+$app->options('/api/catalogue', function (Request $request, Response $response, $args) {
+    $response = $response->withHeader("Access-Control-Max-Age", 600); 
+    return addHeaders($response);
+});
+
+$app->get('/api/catalogue/{filtre}', function (Request $request, Response $response, $args) {
+    $filtre = $args['filtre'];
+    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+   
+    if ($filtre) {
+      $data = json_decode($flux, true); 
+    	
+        $res = array_filter($data, function($obj) use ($filtre)
+        { 
+            return strpos($obj["titre"], $filtre) !== false;
+        });
+        $response->getBody()->write(json_encode(array_values($res)));
+    } else {
+         $response->getBody()->write($flux);
+    }
+    return addHeaders($response);
+});
+
+$app->get('/api/catalogue', function (Request $request, Response $response, $args) {
+    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]'; 
+    $response->getBody()->write($flux);
+
+    return addHeaders($response);
+});
 
 $options = [
     "attribute" => "token",
@@ -75,7 +130,7 @@ $options = [
     "algorithm" => ["HS256"],
     "secret" => JWT_SECRET,
     "path" => ["/api"],
-    "ignore" => ["/api/hello","/api/login","/api/createUser"],
+    "ignore" => ["/api/hello","/api/login"],
     "error" => function ($response, $arguments) {
         $data = array('ERREUR' => 'Connexion', 'ERREUR' => 'JWT Non valide');
         $response = $response->withStatus(401);
